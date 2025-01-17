3eb08c28e8a84a2d4cdcef75ff21a4a13a9e9870
Rokon Khan <rokonjust@gmail.com>
2025-01-18 00:38:42 +0600

    Login User Sytem Successfully Done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,76 @@
-import { useState } from "react";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useContext, useState } from "react";
+import toast from "react-hot-toast";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "../authprovider/AuthProvider";
 
 const Login = () => {
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+  const { signIn, setForgetEmail } = useContext(AuthContext);
+  const location = useLocation();
+  const navigate = useNavigate();
   const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState("");
+  console.log("location in the login page", location);
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log(e.currentTarget);
+    const form = new FormData(e.currentTarget);
+    const email = form.get("email");
+    const password = form.get("password");
+    console.log(email, password);
+
+    signIn(email, password)
+      .then((result) => {
+        console.log(result.user);
+
+        // navigate after login
+        navigate(location?.state ? location.state : "/");
+      })
+      .catch((error) => {
+        console.error(error);
+        // Handle errors with if-else
+        if (error.code === "auth/user-not-found") {
+          setError("No user found with this email.");
+          toast.error("An invalid Email or Password");
+        } else {
+          setError("An unexpected error occurred. Please try again.");
+          toast.error("An invalid Email or Password, Please try again.");
+        }
+      });
+  };
+
+  const handleEmailChange = (e) => {
+    setForgetEmail(e.target.value);
+  };
+
+  const handleSigWithGoogle = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const token = credential.accessToken;
+        // The signed-in user info.
+        const user = result.user;
+        // IdP data available using getAdditionalUserInfo(result)
+        // ...
+        navigate(location?.state ? location.state : "/");
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.customData.email;
+        // The AuthCredential type that was used.
+        const credential = GoogleAuthProvider.credentialFromError(error);
+        // ...
+      });
+  };
+
   return (
     <div>
       <div className="flex w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 lg:max-w-4xl my-6">
@@ -28,8 +95,8 @@ const Login = () => {
             Welcome back!
           </p>
 
-          <a
-            href="#"
+          <button
+            onClick={handleSigWithGoogle}
             className="flex items-center justify-center mt-4 text-gray-600 transition-colors duration-300 transform border rounded-lg dark:border-gray-700 dark:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600"
           >
             <div className="px-4 py-2">
@@ -56,7 +123,7 @@ const Login = () => {
             <span className="w-5/6 px-4 py-3 font-bold text-center">
               Login with Google
             </span>
-          </a>
+          </button>
 
           <div className="flex items-center justify-between mt-4">
             <span className="w-1/5 border-b dark:border-gray-600 lg:w-1/4"></span>
@@ -71,61 +138,64 @@ const Login = () => {
             <span className="w-1/5 border-b dark:border-gray-400 lg:w-1/4"></span>
           </div>
 
-          <div className="mt-4">
-            <label
-              className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200"
-              htmlFor="LoggingEmailAddress"
-            >
-              Email Address
-            </label>
-            <input
-              id="LoggingEmailAddress"
-              className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
-              type="email"
-            />
-          </div>
-
-          <div className="mt-4">
-            <div className="flex justify-between">
+          <form onSubmit={handleLogin}>
+            <div className="mt-4">
               <label
                 className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200"
-                htmlFor="loggingPassword"
+                htmlFor="LoggingEmailAddress"
               >
-                Password
+                Email Address
               </label>
-            </div>
-
-            <div className="relative">
               <input
-                id="loggingPassword"
+                id="LoggingEmailAddress"
                 className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
-                type={showPassword ? "text" : "password"}
-                name="password"
+                type="email"
+                name="email"
               />
-              <p
-                onClick={() => setShowPassword(!showPassword)}
-                className="btn btn-xs absolute right-2 top-2"
-              >
-                {showPassword ? (
-                  <FaEyeSlash className="text-black"></FaEyeSlash>
-                ) : (
-                  <FaEye className="text-black"></FaEye>
-                )}
-              </p>
-              <a
-                href="#"
-                className="text-xs text-blue-400 dark:text-gray-300 hover:underline"
-              >
-                Forget Password?
-              </a>
             </div>
-          </div>
 
-          <div className="mt-6">
-            <button className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
-              Login
-            </button>
-          </div>
+            <div className="mt-4">
+              <div className="flex justify-between">
+                <label
+                  className="block mb-2 text-sm font-medium text-gray-600 dark:text-gray-200"
+                  htmlFor="loggingPassword"
+                >
+                  Password
+                </label>
+              </div>
+
+              <div className="relative">
+                <input
+                  id="loggingPassword"
+                  className="block w-full px-4 py-2 text-gray-700 bg-white border rounded-lg dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring focus:ring-blue-300"
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                />
+                <p
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="btn btn-xs absolute right-2 top-2"
+                >
+                  {showPassword ? (
+                    <FaEyeSlash className="text-black"></FaEyeSlash>
+                  ) : (
+                    <FaEye className="text-black"></FaEye>
+                  )}
+                </p>
+                <a
+                  href="#"
+                  className="text-xs text-blue-400 dark:text-gray-300 hover:underline"
+                >
+                  Forget Password?
+                </a>
+              </div>
+            </div>
+
+            <div className="mt-6">
+              <button className="w-full px-6 py-3 text-sm font-medium tracking-wide text-white capitalize transition-colors duration-300 transform bg-gray-800 rounded-lg hover:bg-gray-700 focus:outline-none focus:ring focus:ring-gray-300 focus:ring-opacity-50">
+                Login
+              </button>
+            </div>
+          </form>
 
           <div className="flex items-center justify-between mt-4">
             <span className="w-1/5 border-b dark:border-gray-600 md:w-1/4"></span>
