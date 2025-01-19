0ef2e127e5e7fe059c800fafbdaffd9457656fac
Md Rokonuzzaman <rokonjust@gmail.com>
2025-01-19 17:47:29 +0600

    The Dashboard Layout Added

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import MainLaout from "../layout/MainLayout";
 
 import Error from "../components/Error";
+import DashboardLayout from "../layout/DashboardLayout";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
@@ -23,6 +24,69 @@ const router = createBrowserRouter([
         path: "/login",
         element: <Login></Login>,
       },
+    ],
+  },
+
+  {
+    path: "/dashboard",
+    element: (
+      <DashboardLayout />
+      // <PrivateRoute>
+      // </PrivateRoute>
+    ),
+    children: [
+      // {
+      //   index: true,
+      //   element: (
+      //     <PrivateRoute>
+      //       <Statistics />
+      //     </PrivateRoute>
+      //   ),
+      // },
+      // {
+      //   path: 'add-plant',
+      //   element: (
+      //     <PrivateRoute>
+      //       <AddPlant />
+      //     </PrivateRoute>
+      //   ),
+      // },
+      // {
+      //   path: 'my-inventory',
+      //   element: (
+      //     <PrivateRoute>
+      //       <MyInventory />
+      //     </PrivateRoute>
+      //   ),
+      // },
+      // {
+      //   path: 'manage-users',
+      //   element: (
+      //     <PrivateRoute>
+      //       <ManageUsers />
+      //     </PrivateRoute>
+      //   ),
+      // },
+      // {
+      //   path: 'profile',
+      //   element: (
+      //     <PrivateRoute>
+      //       <Profile />
+      //     </PrivateRoute>
+      //   ),
+      // },
+      // {
+      //   path: 'my-orders',
+      //   element: (
+      //     <PrivateRoute>
+      //       <MyOrders />
+      //     </PrivateRoute>
+      //   ),
+      // },
+      // {
+      //   path: 'manage-orders',
+      //   element: <ManageOrders />,
+      // },
     ],
   },
 
